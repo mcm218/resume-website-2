3e25b154615873eb08027a80cb06bc5748137b2e
Michael Muniz <mcmuniz218@gmail.com>
2023-11-21 09:34:55 -0500

    continue working on CI

--- envLoader.ts
+++ envLoader.ts
@@ -1,13 +1,13 @@
 var fs = require('node:fs');
+
+// Dev environment.ts
 // Configure Angular `environment.ts` file path
-const targetPath = './src/environments/environment.ts';
+let targetPath = './src/environments/environment.ts';
 // Load node modules
 const dotenv = require('dotenv');
-const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
-console.log(process.env.NODE_ENV, envFile);
-dotenv.config({ path: envFile });
+dotenv.config({ path: '.env' });
 // `environment.ts` file structure
-const envConfigFile = `export const environment = {
+let envConfigFile = `export const environment = {
     apiUrl: '${process.env.API_URL}',
     production: ${process.env.PRODUCTION},
 };`;
@@ -19,5 +19,23 @@ fs.writeFile(targetPath, envConfigFile, function (err: any) {
         throw console.error(err);
     } else {
         console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+    }
+});
+
+// Prod environment.prod.ts
+targetPath = './src/environments/environment.prod.ts';
+dotenv.config({ path: '.env.prod' });
+envConfigFile = `export const environment = {
+    apiUrl: '${process.env.API_URL}',
+    production: ${process.env.PRODUCTION},
+};`;
+
+console.log('The file `environment.prod.ts` will be written with the following content: \n');
+console.log(envConfigFile);
+fs.writeFile(targetPath, envConfigFile, function (err: any) {
+    if (err) {
+        throw console.error(err);
+    } else {
+        console.log(`Angular environment.prod.ts file generated correctly at ${targetPath} \n`);
     }
 });
